693dc7b13daff0889ecd8fa4944754889aa2c84e
Mariam7AG <128908862+Mariam7AG@users.noreply.github.com>
2023-04-10 08:59:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+// alert("Welcome to my website!");
+
+
+
+
+
+// alert("Error!Please enter a valid password.");
+
+
+
+
+
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+
+
+
+
+// alert("Welcome to JS Land...");
+
+// const checkbox = document.createElement('input');
+// checkbox.type = 'checkbox';
+// checkbo`x.id = 'checkbox';
+
+// const label = document.createElement('label');
+// label.innerText = 'Prevent this page from creating additional dialogs.';
+// label.style.display = 'inline-flex';
+// label.style.alignItems = 'center';
+// label.style.gap = '5px';
+// label.insertBefore(checkbox, label.firstChild);
+// const p = document.createElement('p');
+// p.innerText = 'Happy Coding!';
+
+// const div = document.createElement('div');
+// div.appendChild(p);
+// div.appendChild(label);
+
+// const submitBtn = document.createElement('button');
+// submitBtn.innerText = 'OK';
+
+// submitBtn.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   document.body.removeChild(alertBox);
+// });
+
+// div.appendChild(submitBtn);
+
+// const alertBox = document.createElement('div');
+// alertBox.style.position = 'fixed';
+// alertBox.style.zIndex = '1';
+// alertBox.style.top = '10%';
+// alertBox.style.left = '50%';
+// alertBox.style.transform = 'translate(-50%, -50%)';
+// alertBox.style.background = 'white';
+// alertBox.style.padding = '20px';
+// alertBox.style.border = '3px solid blue';
+
+// alertBox.appendChild(div);
+// document.body.appendChild(alertBox);`
