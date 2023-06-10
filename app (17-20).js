7674a898c1ed3e72b639addbc9ec9380249c5d53
Mariam7AG <128908862+Mariam7AG@users.noreply.github.com>
2023-06-10 22:41:34 +0500

    Add files via upload

---
+++ app (17-20).js
@@ -0,0 +1,137 @@
+// let emptyArray = [[]]
+
+
+
+
+
+// let matrixArray = [ [0, 1, 2, 3],[1, 0, 1, 2],[2, 1, 0, 1] ]
+
+
+
+
+
+
+
+// for (var i = 0; i < 11 ; i++){
+//     document.write(i + "<br>")  
+// }
+
+
+
+
+
+
+
+// var tableToPrint = +prompt("Enter a no to show its multiplication table")
+// var lenth = +prompt("Enter Length MUltiplication table")
+// for ( var i= 1; i <=length ; i++ ){
+//     document.write(tableYouToPrint + "x" + i + "=" + (tableToPrint * i) + "<br>")
+// }
+
+
+
+
+
+
+
+// var fruits = [ 'apple' ,'banana','mango','orange','strawberry']
+// for (var i = 0; i < fruits.length ; i++){
+//     document.write(fruits[i] + "<br>") 
+// }
+// for (var i = 0; i < fruits.length ; i++){
+// document.write("Element at index " + i +  " is " + fruits[i] + "<br>")
+// }
+
+
+
+
+
+
+
+// for ( var i= 1; i < 16; i++ ){
+//         document.write(i + "<br>")
+//}
+                    
+// for ( var i= 10; i > 0; i-- ){
+//     document.write(i + "<br>")
+//}
+                    
+// for ( var i= 0; i < 21; i++ ){
+//     if( i%2 == 0){
+//     document.write(i + "<br>")
+//     }
+//  }
+                
+// for ( var i= 0; i < 21; i++ ){
+//      if( i%2 !== 0){
+//      document.write(i + "<br>")
+//     }
+//  }
+                   
+// for ( var i= 1; i < 21; i++ ){
+//       if( i%2 == 0){
+//       document.write(i + "k" +"<br>")
+//     }
+//  }
+
+
+
+
+
+
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var b = prompt("Welcome to ABC Bakery.What do you want to order janaab?")
+// var flag = false
+// for(var i = 0; i < A.length; i++){
+// if ( b == A[i] ){
+//    flag = true
+//    document.write( b + " is avalaible at index " + i + " in our bakery" + "<br>")
+//    break
+//     }
+// }
+// if (flag == false){
+//    document.write(" we r v sorry " + b + " is'nt available in our bakery")
+// }
+
+
+
+
+
+
+
+// var aA = [24, 53, 78, 91, 12]
+// var large = aA[0]
+// for (let i = 1; i < aA.length; i++) {
+//  if (aA[i] > large) {
+//    large = aA[i]
+//  }
+// }
+// console.log("The largest number is: " + large)
+
+
+
+
+
+
+
+// var aA = [24, 53, 78, 91, 12]
+// var large = aA[0]
+// for (let i = 1; i < aA.length; i++) {
+//  if (aA[i] < large) {
+//    large = aA[i]
+//  }
+// }
+// console.log("The largest number is: " + large)
+
+
+
+
+
+
+
+// for ( var i= 1; i < 101; i++ ){
+//   if( i%5 == 0){
+//   document.write(i + " , ")
+//   }
+// }
